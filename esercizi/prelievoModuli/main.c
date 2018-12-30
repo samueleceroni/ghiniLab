dec728dbe04e6daa7a7e2b97c3c855d914fac090
Samuele Ceroni <samuele.ceroni@gmail.com>
2018-12-30 18:36:13 +0100

    create template pthread and reorganized folders

---
+++ esercizi/prelievoModuli/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <errno.h>
+#include <string.h>
+#include "printerror.h"
+
+pthread_mutex_t sync_lock;
+pthread_cond_t sync_cond;
+int n, m;
+
+
+void * func( void *arg){
+	int timeToWait = *((int*) arg), rc;
+	rc = pthread_mutex_lock(&sync_lock); 
+	if( rc ) PrintERROR_andExit(rc,"pthread_mutex_lock failed");
+	// initial setup
+	rc = pthread_mutex_unlock (&sync_lock);
+	if( rc ) PrintERROR_andExit(rc,"pthread_mutex_unlock failed");
+
+	printf("thread initialized\n");
+	while(1){
+		rc = pthread_mutex_lock(&sync_lock); 
+		if( rc ) PrintERROR_andExit(rc,"pthread_mutex_lock failed");
+		//roba in mutua esclusione
+		rc = pthread_mutex_unlock (&sync_lock);
+		if( rc ) PrintERROR_andExit(rc,"pthread_mutex_unlock failed");
+	}
+}
+
+/*
+rc = pthread_cond_wait(&sync_cond, &sync_lock);
+if( rc ) PrintERROR_andExit(rc, "pthread_cond_wait failed");
+
+rc = pthread_cond_signal (&sync_cond);
+if( rc ) PrintERROR_andExit(rc,"pthread_cond_signal failed");
+
+rc = pthread_mutex_lock(&sync_lock); 
+if( rc ) PrintERROR_andExit(rc,"pthread_mutex_lock failed");
+
+rc = pthread_mutex_unlock (&sync_lock);
+if( rc ) PrintERROR_andExit(rc,"pthread_mutex_unlock failed");
+
+*/
+
+int main(){
+	char buffer[500];
+	pthread_t tid;
+	int rc;
+	printf("Init prob\n");
+	//init variables
+	m = 10;
+	n = 25;
+	rc = pthread_cond_init(&sync_cond, NULL);
+	if( rc ) PrintERROR_andExit(rc,"pthread_cond_init failed");
+	rc = pthread_mutex_init(&sync_lock, NULL); 
+	if( rc ) PrintERROR_andExit(rc,"pthread_mutex_init failed");
+
+	printf("Creating threads\n");
+	rc = pthread_create( & tid, NULL, impiegata, (void*)&t1);
+	if( rc !=0) {
+		strerror_r(rc, buffer, 500);
+		printf("%s\n", buffer);
+		exit(1);
+	}
+	for(int i = 0; i<n; i++){
+		rc = pthread_create( & tid, NULL, func, (void*)&t1);
+		if( rc !=0) {
+			strerror_r(rc, buffer, 500);
+			printf("%s\n", buffer);
+			exit(1);
+		}
+	}
+	rc = pthread_cond_signal (&sync_cond);
+	if( rc ) PrintERROR_andExit(rc,"pthread_cond_signal failed");
+
+	pthread_exit(0);
+}
